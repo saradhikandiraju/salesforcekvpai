a934988425857e00c80b519cddf089c827818043
saradhikandiraju <saradhikandiraju@gmail.com>
2026-01-11 19:26:14 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import { BLOG_POSTS } from '@/constants'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://sfai.netlify.app'
+  const baseUrl = 'https://kvpai.netlify.app'
   
   const routes = [
     '',
